b8cefbcdddd0e8f60033cce2aea9aae9b4005890
Matheus Henrique Dorelli Bragatti <matheushdb@hotmail.com>
2021-10-09 14:58:14 -0300

    First commit

---
+++ src/main/java/com/matheushdb/projectapirestdio/ProjectapirestdioApplication.java
@@ -0,0 +1,13 @@
+package com.matheushdb.projectapirestdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectapirestdioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectapirestdioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheushdb/projectapirestdio/ProjectapirestdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheushdb.projectapirestdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectapirestdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
